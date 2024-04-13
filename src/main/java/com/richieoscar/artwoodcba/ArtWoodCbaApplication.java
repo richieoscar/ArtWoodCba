1ee8492a9a4e1f8fdf0543ec2c828897799f554c
Oscar Anyiam <anyiam@systemspecs.com.ng>
2024-04-14 00:48:16 +0100

    Initial commit

---
+++ src/main/java/com/richieoscar/artwoodcba/ArtWoodCbaApplication.java
@@ -0,0 +1,13 @@
+package com.richieoscar.artwoodcba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtWoodCbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtWoodCbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richieoscar/artwoodcba/ArtWoodCbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richieoscar.artwoodcba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtWoodCbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
